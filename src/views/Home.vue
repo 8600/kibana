48036d6178d767b0a6370bc9d80f8aeb2e8ba90e
PUGE <100284685@qq.com>
2018-09-03 18:45:00 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-if="show" class="home">
+    <Datepicker :date="startTime" :option="startTimeOption" @change="startTimeChange" />
     <Chart v-model="mock" :size="{w: 1400, h: 800}"></Chart>
   </div>
 </template>
@@ -7,30 +8,61 @@
 <script>
 import 'echarts'
 import Chart from 'echarts-middleware'
+import Datepicker from 'date-picker-owo'
 const elasticsearch = require('elasticsearch')
 export default {
   name: 'home',
   data () {
     return {
       show: false,
+      startTime: {
+        time: ''
+      },
+      startTimeOption: {
+        type: 'day',
+        placeholder: '起始时间',
+        week: ['yi', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+        month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        format: 'YYYY-MM-DD',
+        inputStyle: {
+          'display': 'inline-block',
+          'padding': '6px',
+          'line-height': '22px',
+          'font-size': '16px',
+          'border': '2px solid #fff',
+          'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+          'border-radius': '2px',
+          'color': '#5F5F5F'
+        },
+        color: {
+          header: '#ccc',
+          headerText: '#f00'
+        }
+      },
       mock: {
         "xAxis": {
           "type": "category", 
           "boundaryGap": false, 
-          "data": [
-            'sd'
-          ]
-        }, 
+          "data": []
+        },
+        "tooltip": {
+          "trigger": 'axis'
+        },
         "yAxis": {
+          "name" : '访问量(次)',
           "type": "value", 
           "boundaryGap": [
             0, 
             "100%"
           ]
-        }, 
+        },
+        "dataZoom": [{
+          "start": 0,
+          "end": 100
+        }],
         "series": [
           {
-            "name": "模拟数据", 
+            "name": "攻击次数", 
             "type": "line", 
             "smooth": true, 
             "symbol": "none", 
@@ -39,18 +71,16 @@ export default {
               "normal": {
                 "color": "rgb(255, 70, 131)"
               }
-            }, 
-            "data": [
-              {"name": 'sdsd', "value": 12},
-              {"name": 'df', "value": 112}
-            ]
+            },
+            "data": []
           }
         ]
       }
     }
   },
   components: {
-    Chart
+    Chart,
+    Datepicker
   },
   created () {
     const client = new elasticsearch.Client({
@@ -61,17 +91,17 @@ export default {
     })
     client.msearch({
       body: [
-        // match all query, on all indices and types
         {},
         { 
           size: 100,
           aggs: {
-            "data": {
-              "date_histogram": {
+            data: {
+              date_histogram: {
                 field: "@timestamp",
-                interval: "12h",
-                time_zone: "Asia/Shanghai",
-                min_doc_count: 1
+                interval: "1d",
+                time_zone: "+08:00",
+                min_doc_count: 0, // 强制返回空 buckets
+                format: "yyyy-MM-dd"
               }
             }
           },
@@ -81,9 +111,9 @@ export default {
                 {
                   range: {
                     "@timestamp":{
-                      gte: 1525795200000,
+                      gte: 1514736000000,
                       lte: 1530374399999,
-                      format:"epoch_millis"
+                      format: "epoch_millis"
                     }
                   }
                 }
@@ -105,6 +135,11 @@ export default {
       this.show = true
       console.log(res.responses[0].aggregations.data.buckets)
     })
+  },
+  methods: {
+    startTimeChange (data) {
+      console.log(data)
+    }
   }
 }
 </script>
